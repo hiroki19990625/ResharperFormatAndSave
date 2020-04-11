fabcb3836094975a24ef76e188891a6838fa3735
hiroki19990625 <hiroki19990625@gmail.com>
2020-04-12 00:43:36 +0900

    プロジェクト ファイルを追加します。

---
+++ ResharperFormatAndSave/Command1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace ResharperFormatAndSave
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class Command1
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("e69fa39d-c589-4f98-8ace-99401b5f4060");
+
+        public static string FormatCommand = string.Empty;
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        private readonly string[] _anyExtensions = new[] {".cs", ".hpp", ".cpp", ".h", ".c", ".xml"};
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Command1"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private Command1(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static Command1 Instance { get; private set; }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get { return this.package; }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in Command1's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService =
+                await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new Command1(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            Task<object> task = ServiceProvider.GetServiceAsync(typeof(DTE));
+            DTE dte = ThreadHelper.JoinableTaskFactory.Run(() => task) as DTE;
+            Task<object> shellTask = ServiceProvider.GetServiceAsync(typeof(IVsUIShell));
+            IVsUIShell shell = ThreadHelper.JoinableTaskFactory.Run(() => shellTask) as IVsUIShell;
+
+            string ext = Path.GetExtension(dte.ActiveDocument.FullName);
+            if (_anyExtensions.Contains(ext))
+            {
+                // dte.ExecuteCommand();
+                Command command = dte.Commands.Item("ReSharper.ReSharper_ReformatCode");
+                Guid guid = new Guid(command.Guid);
+
+                FormatCommand = command.Guid;
+
+                object args = new object();
+                shell?.PostExecCommand(ref guid, (uint) command.ID, 0, ref args);
+            }
+
+            dte.ActiveDocument.Save();
+        }
+    }
+}
